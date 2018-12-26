977e167b7662dfca663bcbaaf837d17b44158926
jecshcier <alsk123111@icloud.com>
2018-12-26 12:55:17 +0800

    update

--- dev/chat.js
+++ dev/chat.js
@@ -2,19 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Tree, Button, Radio, Icon, Modal, Input} from 'antd';
 import './css/chat.css'
-
-class Main extends React.Component{
-  constructor(props){
-    super(props)
-  }
-
-  render() {
-      return []
-  }
-}
-
+import store from "./redux/store"
+import Provider from "react-redux/es/components/Provider"
+import Chat from "./components/chat/chat"
 
 ReactDOM.render(
-  <Main/>,
+  <Provider store={store}>
+    <Chat/>
+  </Provider>,
   document.getElementById('main')
 )
---
+++ dev/components/chat/chat.js
@@ -0,0 +1,19 @@
+import React from "react"
+import ChatView from "./ChatView"
+
+class Chat extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  componentDidMount(){
+
+  }
+
+  render(){
+    return <div>
+      <ChatView />
+    </div>
+  }
+
+}
